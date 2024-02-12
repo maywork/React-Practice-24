da4ab90d907fee37da899393101635a2521c5b91
Mayesha Hridula <mayesha.fairuz.work@gmail.com>
2024-02-12 19:08:02 +0600

    added react library & JSX syntaxes

--- 01vitebasics/src/App.jsx
+++ 01vitebasics/src/App.jsx
@@ -1,10 +1,12 @@
 import Test from "./Test"
 
 function App() {
+  const username = 'Hitesh Chowdhary'
 
   return (
     <>
       <h1>React with vite | Maywork</h1>
+      <h2>Chai aur react with {username}</h2>
       <Test />
     </>
   )
--- 01vitebasics/src/main.jsx
+++ 01vitebasics/src/main.jsx
@@ -2,8 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// function MyApp() {
+//   return (
+//     <div>
+//       <h1>Custom App</h1>
+//     </div>
+//   )
+// }
+
+// const anotherElement = (
+//   <a href='https://google.com' target='_blank' rel='noreferrer'>Visit Google</a>
+// )
+
+// const reactElement = React.createElement(
+//   'a',
+//   { href: 'https://google.com', target: '_blank' },
+//   'clcik me to visit google'
+// )
+
 ReactDOM.createRoot(document.getElementById('root')).render(
+  // 
+  //   <MyApp />
+  // < App />
+  // reactElement
+  // </React.StrictMode>,
+  /* anotherElement */
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    < App />
+  </React.StrictMode>
 )
