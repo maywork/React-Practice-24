dbe21958dac5dc9cecd9041406df4c224dc1677c
Mayesha Hridula <mayesha.fairuz.work@gmail.com>
2024-02-16 02:29:09 +0600

    added tailwind config in project building

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-purple-500 text-gray-800 p-4 rounded-xl'>Tailwind Test</h1>
+    </>
+  )
+}
+
+export default App
