6d981001376c22cd1a0c6b6437681692c3e5cc32
Mayesha Hridula <mayesha.fairuz.work@gmail.com>
2024-02-16 23:58:20 +0600

    added data as props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,12 +2,16 @@ import './App.css'
 import Card from './components/Card'
 
 function App() {
+  let myObj = {
+    username: 'May',
+    age: 21
+  }
 
   return (
     <>
       <h1 className='bg-slate-500 text-slate-800  font-bold p-4 rounded-xl mb-4'>Tailwind Test</h1>
 
-      <Card />
+      <Card channel='chaiaurcode' someObj={myObj} />
 
 
     </>
