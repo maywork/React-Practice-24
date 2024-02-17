82f23ee1528aaa3e79880bc4b66ad06ea52dc37c
Mayesha Hridula <mayesha.fairuz.work@gmail.com>
2024-02-17 14:51:40 +0600

    added react prop examples

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,16 +2,14 @@ import './App.css'
 import Card from './components/Card'
 
 function App() {
-  let myObj = {
-    username: 'May',
-    age: 21
-  }
 
   return (
     <>
       <h1 className='bg-slate-500 text-slate-800  font-bold p-4 rounded-xl mb-4'>Tailwind Test</h1>
 
-      <Card channel='chaiaurcode' someObj={myObj} />
+      <Card username='Mayesha' btnTxt='Click me' />
+      <Card username='Hitesh' btnTxt='Visit me' />
+      <Card username='Jitesh' />
 
 
     </>
