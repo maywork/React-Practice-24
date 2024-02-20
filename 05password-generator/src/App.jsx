4acb17d9938a0223e354069ff963c0f48f6cd894
Mayesha Hridula <mayesha.fairuz.work@gmail.com>
2024-02-20 23:48:22 +0600

    added styling to the main component

--- 05password-generator/src/App.jsx
+++ 05password-generator/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useCallback } from 'react'
-import './App.css'
 
 function App() {
   const [length, setLength] = useState(0)
@@ -26,8 +25,9 @@ function App() {
 
   return (
     <>
-      <h1 className='text-4xl text-center text-white'>Password generator</h1>
-      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700'>
+
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-700'>
+        <h1 className='text-4xl text-center text-white my-3'>Password generator</h1>
         <div className='flex shadow rounded-lg overflow-hidden mb-4'>
           <input
             type="text"
